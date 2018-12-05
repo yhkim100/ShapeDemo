d97b9c4c3fc0f47eb351a7b5438a8f8c109b1256
Young-hoon Kim <yhkim.software@gmail.com>
2018-12-05 09:48:29 -0500

    Add files via upload

---
+++ ChartPlotDemo/ChartPlotDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChartPlotDemo
+//
+//  Created by Kim, Young-hoon on 12/4/18.
+//  Copyright © 2018 Garmin International. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
